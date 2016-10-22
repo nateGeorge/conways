d0d48fbe31cd9dff09ea34fc3222d24c44360723
wordsforthewise <wordsforthewise@gmail.com>
2016-10-22 12:31:28 -0600

    :tada::chicken:

--- conway.js
+++ conway.js
@@ -1,8 +1,25 @@
-function createsGame () {
+function createsGame (row, col) {
   var board = new Array
+  board = makeRows(board, row, col)
   return board
 }
 
+function makeRows(board, row, col) {
+  for(var i = 0; i<row; i++){
+    var arow = makeCols(col)
+    board.push(arow)
+  }
+  return board
+}
 
+function makeCols(col) {
+  arow = new Array
+  for(var i = 0; i<col; i++){
+    arow.push(Math.round(Math.random()))
+  }
+  return arow
+}
+
+createsGame(6)
 
 module.exports = createsGame
--- test.js
+++ test.js
@@ -3,9 +3,18 @@ const createsGame = require('./conway.js')
 
 describe('The game of life', () => {
   it('creates the game', function () {
-    const game = createsGame()
+    const game = createsGame(6,6)
     expect(game).to.not.equal(null)
     expect(game).to.not.equal(undefined)
     expect(typeof game).to.equal('object')
+  })
+  it('creates game and checks rows', () => {
+    const game = createsGame(6,6)
+    expect(game.length).to.equal(6)
+  })
+
+  it('creates game and checks columns', () => {
+    const game = createsGame(6,6)
+    expect(game[1].length).to.equal(6)
   })
 });
